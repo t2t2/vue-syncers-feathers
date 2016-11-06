10a787edacfdc40c362d176b23238eebd5bddc61
t2t2 <taavot@gmail.com>
2016-11-06 15:41:44 +0200

    fully removing driverOptions.feathers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {matcher} from 'feathers-commons/lib/utils'
 import aliasesMixinMaker from './aliases'
 import Syncer from './syncer'
 import syncerMixin from './mixin'
-import {warn} from './utils'
 
 const defaults = {
 	aliases: false,
@@ -30,15 +29,6 @@ export default {
 	 */
 	install: function (Vue, options = {}) {
 		const extend = Vue.util.extend
-		// Deprecation Warning: 0.3
-		// Deprecate: 0.4
-		/* istanbul ignore next */
-		if (options.driverOptions && options.driverOptions.feathers) {
-			if (process.env.NODE_ENV !== 'production') {
-				warn('Deprecation warning: driverOptions.feathers to be deprecated in favor of just feathers')
-			}
-			options.feathers = options.driverOptions.feathers
-		}
 		// Vue 2.0 has util.toObject, but 1.0 doesn't
 		options = extend(extend({}, defaults), options)
 
